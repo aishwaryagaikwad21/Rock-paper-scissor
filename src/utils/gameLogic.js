564fd0db60bcf89a6cba82ec04e40ae35915e5f1
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-07-21 17:40:42 +0530

    gameLogic

--- src/utils/gameLogic.js
+++ src/utils/gameLogic.js
@@ -9,19 +9,19 @@ var rsp = () => {
         //generating random value for every player out of stone = 7 scissor = 8 paper = 9
         v[i] = Math.floor(Math.random() * 3) + 7;
     }
-    //console.log(v)
+    console.log(v)
     for (var i = 0; i < 4; i++){
             var first = v[i]
             for (var j = 0; j < 4; j++){
                 var second = v[j]
                 if (i != j) {
-                    if (first == 7 && second == 8) {
+                    if ((first == 7 && second == 8)||(first == 8 && second == 7)) {
                         score[i][j] += 1; //stone vs scissor = stone wins, player[i] wins
                     }
-                    else if (first == 8 && second == 9) {
+                    else if ((first == 8 && second == 9)||(first == 9 && second == 8)) {
                         score[i][j] += 1; //scissor vs paper = scissor wins, player[i] win
                     }
-                    else if (first == 9 && second == 7) {
+                    else if ((first == 9 && second == 7)||(first == 7 && second== 9)) {
                         score[i][j] += 1; //paper vs stone = paper wins, player[i] win
                     }
                     else {
